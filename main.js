2a3231d9162da5e3eeb709647a8e7fac07a2751d
Harish0411 <hariharanramasamy98@gmail.com>
2023-02-28 13:07:10 +0530

    combine-2-arrays

---
+++ main.js
@@ -0,0 +1,14 @@
+// combine two arrays
+
+let m=[2,3,4,"hari", 19,45];
+let n=[6,7,8];
+let o=[];
+for(i=0;i<m.length;i++){
+     o.push(m[i]);
+}
+for(j=0;j<n.length;j++){
+     o.push(n[j]);
+}
+console.log(m);
+console.log(n);
+console.log(o);
